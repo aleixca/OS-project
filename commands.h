782e7e80a8b2e94bee4fbe14810f245a9fb3b189
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-05 18:01:47 +0100

    Refactor command definitions and parse_command function

--- commands.h
+++ commands.h
@@ -1,15 +1,18 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+#include <string.h>
+#include <ctype.h>
+
 #define CMD_UNKNOWN            0
 #define CMD_LIST_REALMS        1
 #define CMD_LIST_PRODUCTS      2
 #define CMD_START_TRADE        3
 #define CMD_EXIT               4
-#define CMD_OK_OTHER           5
+#define CMD_OK                 5
 #define CMD_INCOMPLETE         6
 
 
-int parse_command(char **argv, int argc);
+int parse_command(char *command);
 
 #endif
