a5ff96fc05eee90a16163314694156d2934ba23b
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-05 18:02:42 +0100

    Refactor to_upper function and add read_line function

--- io.c
+++ io.c
@@ -1,13 +1,40 @@
 #include "io.h"
-#include <unistd.h>
-#include <string.h>
-#include <ctype.h>
+
 
 
 void to_upper(char *s) {
-    for (size_t i = 0; s[i] != '\0'; i++) {
-        s[i] = (char)toupper((unsigned char)s[i]);
+    for (int i = 0; s[i] != '\0'; i++) {
+        s[i] = (char)toupper((char)s[i]);
     }
 }
+
+
+int read_line(int fd, char *buf, int max) {
+    if (!buf || max < 2) {
+        return 0;
+    }
+
+    int i = 0;
+    while (i < max - 1) {
+        char c;
+        int r = (int)read(fd, &c, 1);
+        
+        if (r <= 0) {
+            break;       // EOF or error
+        }
+
+        buf[i++] = c;
+
+        if (c == '\n'){
+            break;              // Stop at newline
+        }    
+    }
+
+    buf[i] = '\0';
+    return i;
+}
+
+
+
 
 
