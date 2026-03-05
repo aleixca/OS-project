557eab87dba651f9adb1d557dd0dc097dcf89973
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-05 18:01:32 +0100

    Update commands.c

--- commands.c
+++ commands.c
@@ -1,30 +1,23 @@
-#include "parser.h"
-#include <string.h>
-#include <ctype.h>
+#include "commands.h"
 
-int parseCommand(){
-}
 
-void listRealms () {
-}
+int parse_command(char *command){
+    
+    if (strlen(command) == 0) {
+        return CMD_UNKNOWN;
+    }
 
-void pledgeRealm () {
-}
+    to_upper(command);
 
-void pledgeRespond () {
-}
-
-void listProducts () {
-}
-
-void startTrade () {
-}
-
-void pledgeStatus () {
-}
-
-void envoyStatus () {
-}
-
-void exit () {
+    if (strcmp(command, "LIST REALMS") == 0) {
+        return CMD_LIST_REALMS;
+    } else if (strcmp(command, "LIST PRODUCTS") == 0) {
+        return CMD_LIST_PRODUCTS;
+    } else if (strcmp(command, "START TRADE") == 0) {
+        return CMD_START_TRADE;
+    } else if (strcmp(command, "EXIT") == 0) {
+        return CMD_EXIT;
+    } else { // AFEGIR ELS COMMANDS QUE HAN DE FER RETURN DE CMD_OK_OTHER I CMD_INCOMPLETE
+        return CMD_UNKNOWN;
+    }
 }
