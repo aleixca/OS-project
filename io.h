84d01363ed594bb95f0cb8cdc8be61c217fdae36
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-05 18:02:56 +0100

    Add utility functions and includes to io.h

--- io.h
+++ io.h
@@ -1,13 +1,18 @@
 #ifndef IO_H
 #define IO_H
 
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
 #include <stddef.h>
 #include <sys/types.h>
 
 
 
+#define printF(x) write(STDOUT_FILENO, x, strlen(x))
+
 void to_upper(char *str);
 
-
+int read_line(int fd, char *buf, int max);
 
 #endif
