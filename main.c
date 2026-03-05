9f905b538af982d24ccaa189adc5c364d0c0d494
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-05 18:03:43 +0100

    Implement main function with signal handling and loading

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
 
+
+#include "terminal.h"
+#include "signal.h"
+#include "io.h"
+#include "maester.h"
+#include "inventory.h"
+
+
+#include <string.h>
+#include <unistd.h>
+
+
+
+
+int main(int argc, char *argv[]){
+    
+    signal_handler();
+    read_Maester(argv[1]);
+    load_inventory(argv[2]);
+    terminal();
+
+
+
+    return 0;
+}
