3d52b1b5f64dbf655e360a87cbf4ddd1377d527e
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-06 10:39:31 +0100

    Overload terminal function to accept parameters

--- terminal.h
+++ terminal.h
@@ -1,13 +1,16 @@
 #ifndef TERMINAL_H
 #define TERMINAL_H
+
 #include "commands.h"
 #include "io.h"
 #include "signal.h"
+#include "maester.h"
+#include "inventory.h"
 
 
 #include <string.h>
 #include <unistd.h>
 
-void terminal();
+void terminal(int total_products, Product *products, Maester maester);
 
 #endif
