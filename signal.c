fe925281842c44f7f9ecd35ede3aba61e9afbed5
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-06 10:38:36 +0100

    Refactor signal_handler for improved readability

--- signal.c
+++ signal.c
@@ -3,15 +3,19 @@
 
 int stop_program = 0;
 
-void signal_handler(int sig){
-  (void)sig;
-  printF("Exiting program gracefully...\n");
-   stop_program = 1;
+void signal_handler(int sig) {
+    (void)sig;
+    stop_program = 1;
 
+    printF("\nExiting program gracefully...\n");
 }
 
 void setup_signal(void) {
     signal(SIGINT, signal_handler);
+}
+
+int stop_requested(void) {
+    return stop_program != 0;
 }
 
 
