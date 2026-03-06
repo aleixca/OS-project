70a7457277c4fd8988c678f4b7a6a1d2c7399d0d
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-06 10:36:33 +0100

    Update inventory.c

--- inventory.c
+++ inventory.c
@@ -2,20 +2,20 @@
 
 
 
-void load_inventory(char *argv[]) {
+Product* load_inventory(char *path_2, int *total_products) {
 
-    int fd = open(argv[2], O_RDONLY);
+    int fd = open(path_2, O_RDONLY);
     int i = 0;
     int n;
-    int total_products = 0;
-    Product *products;
+    Product *products = NULL;
 
     if (fd == -1) {
         printF("Error: Could not open inventory file.\n");
-        return;
+        return 0;
     }
     Product temp;
         while (read(fd, &temp, sizeof(Product)) > 0) {
+        
         n = read(fd, temp.name, 100);
         temp.name[n] = '\0'; // Ensure null termination
         
@@ -27,32 +27,56 @@ void load_inventory(char *argv[]) {
         if (new_products == NULL) {
             printF("Error: Could not reallocate memory for products.\n");
             close(fd);
-            return;
+            return 0;
         }
         products = new_products;
         products[i] = temp;
         i++;
     }
-    total_products = i;
+    *total_products = i;
     close(fd);
+    
+    return products;
 }
 
 void list_products(int total_products, Product *products) {
     int i = 0;
-    printF("--- Trade Ledger --- \n");
-    printF("Item                    | Value (Gold) | Weight (Stone)\n");
-    PrintF("-------------------------------------------------------\n");
+    char *output;
+    
+
+    printF("\t\t--- Trade Ledger --- \n");
+    printF("Item                 | Value (Gold) | Weight (Stone)\n");
+    printF("-------------------------------------------------------\n");
+
+
 
     for (i = 0; i < total_products; i++) {
         char *output;
-        int len = asprintf(&output, "\n %s      %d      %.1f ", products[i].name, products[i].amount, products[i].weight);
-        write(1, output, len);
+        int len = asprintf(&output, "%-20s | %10d | %12.1f\n", products[i].name, products[i].amount, products[i].weight);
+        if(len == -1) {
+            printF("Error creating output string\n");
+            return;
+        }
+        printF(output);
         free(output);
     }
-    printF("\n-------------------------------------------------------\n");
-    printF("Total Entries: %d\n", total_products);
+    printF("\n-------------------------------------------------------\n");  
+    int len = asprintf(&output, "Total Products: %d\n", total_products);
+    if(len == -1) {
+        printF("Error creating output string\n");
+        return;
+    }
+    printF(output);
+    free(output);
 
 }
+
+void free_inventory(Product *products) {
+    
+    free(products);
+    products = NULL;
+}
+
 
 void free_inventory(Product *products) {
     free(products);
