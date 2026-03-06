1d1b29d248de16524abcfcf9604160b6594bd4f6
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-06 10:38:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
-
-
+#define _GNU_SOURCE
 #include "terminal.h"
 #include "signal.h"
 #include "io.h"
@@ -7,6 +6,8 @@
 #include "inventory.h"
 
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -14,12 +15,27 @@
 
 
 int main(int argc, char *argv[]){
-    
-    signal_handler();
-    read_Maester(argv[1]);
-    load_inventory(argv[2]);
-    terminal();
+    int total_products = 0;
+    Product *products = NULL;
+    Maester maester;
 
+
+    if (argc != 3) {
+        printF("Usage: ./main <maester_config.txt> <inventory.bin>\n");
+        return 1;
+    }
+
+    
+    setup_signal();
+
+        maester = read_Maester(argv[1]);
+        products = load_inventory(argv[2], &total_products);
+        terminal(total_products, products, maester);
+    
+    
+
+    free_inventory(products);
+    free_Maester(maester);
 
 
     return 0;
