a422402def9e4dbd73fa10b2ed3f5d3dcd17dfcb
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-06 10:37:40 +0100

    Refactor readMaester and add free_Maester function
    Refactor readMaester function to improve readability and memory management. Introduce free_Maester function for proper cleanup.

--- maester.c
+++ maester.c
@@ -1,55 +1,65 @@
 #include "maester.h"
 
 
-
-void readMaester(char **argv){
+Maester read_Maester(char *path){
     Maester maester;
-    Routes *routes;
+    Route *routes = NULL;
     char *buf;
     int i = 0;
+    char *output;
 
-    int fd = open(argv[0], O_RDONLY);
+    int fd = open(path, O_RDONLY);
     if (fd == -1) {
         printF("Error opening file\n");
-        return;
+        Maester empty_maester = {0};
+        return empty_maester;
     }
 
-    read_lines(fd, buf, 100);
-    maester.realm_name = strdup(buf);
-    read_lines(fd, buf, 100);
+
+  
+    buf = readUntil(fd, '\n');
+    printF(buf);
+    maester.realm_name = buf;
+    buf = readUntil(fd, '\n');
     maester.user_dir = strdup(buf);
-    read_lines(fd, buf, 100);
+    buf = readUntil(fd, '\n');
     maester.envoy_count = atoi(buf);
-    read_lines(fd, buf, 100);
+    buf = readUntil(fd, '\n');
     maester.listen_ip = strdup(buf);
-    read_lines(fd, buf, 100);
+    buf = readUntil(fd, '\n');
     maester.listen_port = atoi(buf);
-    read_lines(fd, buf, 100);
-    read_lines(fd, buf, 100);
-    
-    while (read_lines(fd, buf, 100) > 0) {
-        Route route;
-        route.maester = strdup(buf);
-        read_lines(fd, buf, 100);
-        route.ip = strdup(buf);
-        read_lines(fd, buf, 100);
-        route.port = atoi(buf);
+    buf = readUntil(fd, '\n');
+
+    while (readUntil(fd, '\n') != NULL) {
+        Route temp;
+        temp.maester = strdup(buf);
+        buf = readUntil(fd, '\n');
+        temp.ip = strdup(buf);
+        buf = readUntil(fd, '\n');
+        temp.port = atoi(buf);
 
         Route *new_routes = realloc(routes, (maester.envoy_count + 1) * sizeof(Route));
         if (new_routes == NULL) {
             printF("Error reallocating memory for routes\n");
             free(routes);
             close(fd);
-            return;
+            return maester;
         }
         routes = new_routes;
-        routes[i] = route;
+        routes[i] = temp;
         i++;
     }
     maester.route_count = i;
     maester.routes = routes;
     close(fd);
 
+    int len = asprintf(&output, "Maester of %s initialized. The board is set.\n", maester.realm_name);
+    if (len != -1) {
+        printF(output);
+        free(output);
+    }
+
+    return maester;
 }
 
 
@@ -60,11 +70,27 @@ void list_realms(Maester maester) {
     for(i = 0; i < maester.route_count; i++) {
         char *output;
         int len = asprintf(&output, "\n - %s", maester.routes[i].maester);
+        if(len == -1) {
+            printF("Error creating output string\n");
+            return;
+        }
         printF(output);
         free(output);
     }
 
 }
+
+
+void free_Maester(Maester maester) {
+    free(maester.realm_name);
+    free(maester.user_dir);
+    for (int i = 0; i < maester.route_count; i++) {
+        free(maester.routes[i].maester);
+        free(maester.routes[i].ip);
+    }
+    free(maester.routes);
+}
+
 
 
 
