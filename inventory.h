29b1da76bb726d71022b4d509ea04db1f035d68a
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-06 10:36:50 +0100

    Refactor inventory.h for improved includes and function

--- inventory.h
+++ inventory.h
@@ -1,11 +1,15 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
+#define _GNU_SOURCE
 
-#include 'io.h'
+#include "io.h"
+#include "maester.h"
 
-#include <stdlib.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
+
 
 typedef struct {
     char name[100];
@@ -13,9 +17,7 @@ typedef struct {
     float weight;
 } Product;
 
-int total_products;
-
-void load_inventory(char *argv[]);
+Product* load_inventory(char *path_2,  int *total_products);
 void list_products(int total_products, Product *products);
 void free_inventory(Product *products);
 
