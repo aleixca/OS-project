d705ca8f9d706e62341c17faee187429b6dd462d
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-07 18:29:29 +0100

    Add new functions for string manipulation

--- io.h
+++ io.h
@@ -1,6 +1,7 @@
 #ifndef IO_H
 #define IO_H
 
+#include "signal.h"
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
@@ -13,7 +14,8 @@
 #define printF(x) write(STDOUT_FILENO, x, strlen(x))
 
 void to_upper(char *str);
-
+char *add_newline(char *s);
+void remove_slashr(char *str);
 char *readUntil(int fd, char separator);
 
 char *read_screen();
