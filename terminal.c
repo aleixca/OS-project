8145572a74dabf719762cecbb3ddf3c7e09c33cb
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-07 18:31:21 +0100

    Update terminal.c

--- terminal.c
+++ terminal.c
@@ -1,52 +1,70 @@
 #include "terminal.h"
 
-#include <stdlib.h>
+
+
 
 void terminal(int total_products, Product *products, Maester maester) {
-    char *command;
-    int running = 1;
+    int exit = 0;
+    char *realm = NULL;
 
-    setup_signal();
+    while (!stop_requested() && !exit) {
 
-    while (running && !stop_requested()) {
-        printF("$ ");
-        command = read_screen();
-
-        switch (parse_command(command)) {
-            case CMD_LIST_REALMS:
-                list_realms(maester);
-                printF("\n");
-                break;
-            case CMD_LIST_PRODUCTS:
-                list_products(total_products, products);
-                break;
-            case CMD_LIST_PRODUCTS_REALM:
-                printF("ERROR: You must have an alliance to trade.\n");
-                break;
-            case CMD_PLEDGE:
-                printF("Pledge sent.\n");
-                break;
-            case CMD_PLEDGE_STATUS:
-                printF("$ PLEDGE STATUS\n");
-                break;
-            case CMD_START_TRADE_REALM:
-                printF("Entering trade mode.\n");
-                break;
-            case CMD_START_TRADE:
-                printF("Did you mean to start a trade? Please review syntax: START TRADE <REALM>\n");
-                break;
-            case CMD_INCOMPLETE:
-                printF("Did you mean to send a pledge? Please review syntax.\n");
-                break;
-            case CMD_EXIT:
-                printF("Exiting...\n");
-                running = 0;
-                break;
-            default:
-                printF("Unknown command.\n");
-                break;
-        }
-
-        free(command);
+    switch (parse_command(&realm)) {
+        case CMD_LIST_REALMS:
+            list_realms(maester);
+            break;
+        case CMD_LIST_PRODUCTS_OWN:
+            list_products(total_products, products);
+            break;
+        case CMD_LIST_PRODUCTS_REALM:
+            printF("Command ok\n");
+            break;
+        case CMD_START_TRADE:
+            start_trade(realm);
+            free(realm);
+            break;
+        case CMD_PLEDGE_STATUS:
+            printF("Command ok.\n");
+            break;
+        case CMD_ENVOY_STATUS:
+            printF("Command ok.\n");
+            break;
+        case CMD_PLEDGE_RESPOND_ACCEPT:
+            printF("Command ok.\n");
+            break;
+        case CMD_PLEDGE_RESPOND_REJECT:
+            printF("Command ok.\n");
+            break;
+        case CMD_PLEDGE:
+            printF("Command ok.\n");
+            break;
+        case CMD_UNKNOWN:
+            printF("Unknown command.\n");
+            break;
+        case CMD_INCOMPLETE_LIST:
+            printF("Incomplete command: LIST requires additional arguments. (LIST REALMS or LIST PRODUCTS)\n");
+            break;
+        case CMD_INCOMPLETE_PLEDGE:
+            printF("Incomplete command: PLEDGE requires additional arguments.(PLEDGE STATUS or PLEDGE RESPOND <REALM> ACCEPT/REJECT)\n");
+            break;
+        case CMD_INCOMPLETE_PLEDGE_RESPOND:
+            printF("Incomplete command: PLEDGE RESPOND requires additional arguments.(PLEDGE RESPOND <REALM> ACCEPT/REJECT)\n");
+            break;
+        case CMD_INCOMPLETE_ALLIANCE:
+            printF("Incomplete command: ALLIANCE requires additional arguments. (PLEDGE <REALM> <sigil.jpg>)\n");
+            break;
+        case CMD_INCOMPLETE_TRADE:
+            printF("Incomplete command: TRADE requires additional arguments. (START TRADE <REALM>)\n");
+            break;
+        case CMD_INCOMPLETE_ENVOY:
+            printF("Incomplete command: ENVOY requires additional arguments. (ENVOY STATUS)\n");
+            break;
+        case CMD_EXIT:
+            free(realm);
+            exit_maester(maester);
+            exit = 1;
+            break;
     }
+} 
+
 }
