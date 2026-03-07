3f8188bf29f9ba1293b2b54d4090c9ede3803cb4
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-07 18:30:45 +0100

    Modify signal handler to close STDIN on exit
    Close standard input in signal handler before exiting.

--- signal.c
+++ signal.c
@@ -6,8 +6,7 @@ int stop_program = 0;
 void signal_handler(int sig) {
     (void)sig;
     stop_program = 1;
-
-    printF("\nExiting program gracefully...\n");
+    close(STDIN_FILENO);
 }
 
 void setup_signal(void) {
