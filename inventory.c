e432b8457eb8ea1027f9fe48d2a262a6efd6d609
aleixca <103314058+aleixca@users.noreply.github.com>
2026-03-07 18:28:25 +0100

    Refactor load_inventory and list_products functions
    Updated inventory loading and listing functions for improved memory management and output formatting.

--- inventory.c
+++ inventory.c
@@ -1,41 +1,33 @@
 #include "inventory.h"
 
 
-
 Product* load_inventory(char *path_2, int *total_products) {
-
     int fd = open(path_2, O_RDONLY);
     int i = 0;
-    int n;
     Product *products = NULL;
+    Product temp;
 
     if (fd == -1) {
         printF("Error: Could not open inventory file.\n");
-        return 0;
+        return NULL;
     }
-    Product temp;
-        while (read(fd, &temp, sizeof(Product)) > 0) {
-        
-        n = read(fd, temp.name, 100);
-        temp.name[n] = '\0'; // Ensure null termination
-        
-        read (fd, &temp.amount, sizeof(int));
-        
-        read(fd, &temp.weight, sizeof(float));
 
+    while (read(fd, &temp, sizeof(Product)) != 0) {
         Product *new_products = realloc(products, (i + 1) * sizeof(Product));
         if (new_products == NULL) {
             printF("Error: Could not reallocate memory for products.\n");
+            free(products);
             close(fd);
-            return 0;
+            return NULL;
         }
+
         products = new_products;
         products[i] = temp;
         i++;
     }
-    *total_products = i;
+
     close(fd);
-    
+    *total_products = i;
     return products;
 }
 
@@ -44,15 +36,18 @@ void list_products(int total_products, Product *products) {
     char *output;
     
 
-    printF("\t\t--- Trade Ledger --- \n");
-    printF("Item                 | Value (Gold) | Weight (Stone)\n");
+    printF("--- Trade Ledger --- \n");
+    char *header = NULL;
+    asprintf(&header, "%-25s | %15s | %12s\n","Item", "Value (Gold)", "Weight (Stone)");
+    printF(header);
+    free(header);
     printF("-------------------------------------------------------\n");
 
 
 
     for (i = 0; i < total_products; i++) {
         char *output;
-        int len = asprintf(&output, "%-20s | %10d | %12.1f\n", products[i].name, products[i].amount, products[i].weight);
+        int len = asprintf(&output, "%-25s | %15d | %12.1f\n", products[i].name, products[i].amount, products[i].weight);
         if(len == -1) {
             printF("Error creating output string\n");
             return;
@@ -61,7 +56,7 @@ void list_products(int total_products, Product *products) {
         free(output);
     }
     printF("\n-------------------------------------------------------\n");  
-    int len = asprintf(&output, "Total Products: %d\n", total_products);
+    int len = asprintf(&output, "Total Entries: %d\n", total_products);
     if(len == -1) {
         printF("Error creating output string\n");
         return;
